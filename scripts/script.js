5e6b907e65bd5f10113e4d91a27872167bf1a450
modelingandsawing <142691153+modelingandsawing@users.noreply.github.com>
2023-10-10 23:25:43 +0200

    remove first 188 wedding dresses. fix ui. fix texts. fix colors.

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,31 @@
 // Redirect to another page
 function redirect(url) {
-    window.location.href = url;
-};
+  console.log("🚀 ~ file: script.js:3 ~ redirect ~ url:", url);
+  window.location.href = url;
+}
 
 // Show the language selection menu
 function showLanguageMenu() {
-    document.querySelector('div#select-language').style.display = 'block';
-};
+  document.querySelector("div#change-language").style.visibility = "visible";
+}
 
-// Hide the language selection menu
+function showNavigationMenu() {
+  document.querySelector("div#navigation-menu").style.visibility = "visible";
+}
+
 document.onclick = (e) => {
-    if (e.target.id == 'title-change-language') return;
-    document.querySelector('div#select-language').style.display = 'none';
+  // Hide the language selection menu
+  if (e.target.id !== "icon-change-language") {
+    document.querySelector("div#change-language").style.visibility = "hidden";
+  }
+
+  // Hide the navigation menu
+  if (e.target.id !== "icon-navigation-menu") {
+    document.querySelector("div#navigation-menu").style.visibility = "hidden";
+  }
 };
 
 HTMLElement.prototype.switchClass = function (before, after) {
-    this.classList.remove(before);
-    this.classList.add(after);
+  this.classList.remove(before);
+  this.classList.add(after);
 };
