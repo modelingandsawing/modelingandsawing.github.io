352003df1c17e01238ef628c13f55fdd99002b9a
modelingandsawing <142691153+modelingandsawing@users.noreply.github.com>
2023-10-22 20:22:35 +0200

    feat: hide languages menu on the main page

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,38 @@
 // Redirect to another page
 function redirect(url) {
-  console.log("🚀 ~ file: script.js:3 ~ redirect ~ url:", url);
   window.location.href = url;
 }
 
 // Show the language selection menu
 function showLanguageMenu() {
-  document.querySelector("div#change-language").style.visibility = "visible";
+  const target = document.querySelector("div#change-language");
+  if (target) {
+    target.style.visibility = "visible";
+  }
 }
 
 function showNavigationMenu() {
-  document.querySelector("div#navigation-menu").style.visibility = "visible";
+  const target = document.querySelector("div#navigation-menu");
+  if (target) {
+    target.style.visibility = "visible";
+  }
 }
 
 document.onclick = (e) => {
   // Hide the language selection menu
   if (e.target.id !== "icon-change-language") {
-    document.querySelector("div#change-language").style.visibility = "hidden";
+    const target = document.querySelector("div#change-language");
+    if (target) {
+      target.style.visibility = "hidden";
+    }
   }
 
   // Hide the navigation menu
   if (e.target.id !== "icon-navigation-menu") {
-    document.querySelector("div#navigation-menu").style.visibility = "hidden";
+    const target = document.querySelector("div#navigation-menu");
+    if (target) {
+      target.style.visibility = "hidden";
+    }
   }
 };
 
