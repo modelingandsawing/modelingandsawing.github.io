bb105944e87aa355bdb962dabdb1d21a9c6a5746
modelingandsawing <142691153+modelingandsawing@users.noreply.github.com>
2023-10-22 20:28:51 +0200

    fix: wedding dresses on other languages

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,36 +1,40 @@
 class Slideshow {
-    slideIndex = 1;
+  slideIndex = 1;
 
-    activate = (i=1) => {
-        document.querySelector('div#slideshow').switchClass('inactive', 'active');
-        if (i > 1) {
-            this.slideIndex = i;
-            this.showSlide();
-        };
-    };
+  activate = (i = 1) => {
+    document.querySelector("div#slideshow").switchClass("inactive", "active");
+    if (i > 1) {
+      this.slideIndex = i;
+      this.showSlide();
+    }
+  };
 
-    deactivate = () => {
-        document.querySelector('div#slideshow').switchClass('active', 'inactive');
-    };
+  deactivate = () => {
+    document.querySelector("div#slideshow").switchClass("active", "inactive");
+  };
 
-    switchSlide = (n) => {
-        this.showSlide(this.slideIndex += n);
-    };
+  switchSlide = (n) => {
+    this.showSlide((this.slideIndex += n));
+  };
 
-    showSlide = (n=1) => {
-        let x = document.getElementsByClassName('slideshow-elem');
-        if (n > x.length) { this.slideIndex -= x.length }
-        if (n < 1) { this.slideIndex = x.length };
-        for (let i = 0; i < x.length; i++) {
-            x[i].switchClass('active', 'inactive');
-        };
-        x[this.slideIndex - 1].switchClass('inactive', 'active');
-    };
-};
+  showSlide = (n = 1) => {
+    const x = document.getElementsByClassName("slideshow-elem");
+    if (n > x.length) {
+      this.slideIndex -= x.length;
+    }
+    if (n < 1) {
+      this.slideIndex = x.length;
+    }
+    for (let i = 0; i < x.length; i++) {
+      x[i].switchClass("active", "inactive");
+    }
+    x[this.slideIndex - 1].switchClass("inactive", "active");
+  };
+}
 
 const slideshow = new Slideshow();
 
-document.querySelector('div.controls').addEventListener('click', (e) => {
-    if (!e.target.classList.contains('controls')) return;
-    slideshow.deactivate();
-})
+document.querySelector("div.controls").addEventListener("click", (e) => {
+  if (!e.target.classList.contains("controls")) return;
+  slideshow.deactivate();
+});
